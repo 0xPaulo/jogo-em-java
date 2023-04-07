7a46720b1787bf12868d66bf79328e9f96af8a76
0xPaulo <paulo_czar@outlook.com.br>
2023-04-07 03:17:06 -0300

    primeira_fase_implementada

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -7,8 +7,6 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
-import java.awt.Color;
-
 import main.GamePanel;
 import main.Keyhandler;
 
@@ -36,14 +34,14 @@ public class Player extends Entity {
     public void getPlayerImage() {
         try {
 
-            File f1 = new File("./src/player/boy_up_1.png");
-            File f2 = new File("./src/player/boy_up_2.png");
-            File f3 = new File("./src/player/boy_down_1.png");
-            File f4 = new File("./src/player/boy_down_2.png");
-            File f5 = new File("./src/player/boy_left_1.png");
-            File f6 = new File("./src/player/boy_left_2.png");
-            File f7 = new File("./src/player/boy_right_1.png");
-            File f8 = new File("./src/player/boy_right_2.png");
+            File f1 = new File("./src/res/player/boy_up_1.png");
+            File f2 = new File("./src/res/player/boy_up_2.png");
+            File f3 = new File("./src/res/player/boy_down_1.png");
+            File f4 = new File("./src/res/player/boy_down_2.png");
+            File f5 = new File("./src/res/player/boy_left_1.png");
+            File f6 = new File("./src/res/player/boy_left_2.png");
+            File f7 = new File("./src/res/player/boy_right_1.png");
+            File f8 = new File("./src/res/player/boy_right_2.png");
             up1 = ImageIO.read(f1);
             up2 = ImageIO.read(f2);
             down1 = ImageIO.read(f3);
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -8,6 +8,7 @@ import java.awt.Graphics2D;
 import javax.swing.JPanel;
 
 import entity.Player;
+import tile.TileManager;
 
 public class GamePanel extends JPanel implements Runnable {
 
@@ -16,22 +17,22 @@ public class GamePanel extends JPanel implements Runnable {
     final int scale = 3;
 
     public final int tileSize = originalTileSize * scale; // 48x48 tile
-    final int maxScreenCol = 16;
-    final int maxScreenRow = 12;
-    final int screenWidth = tileSize * maxScreenCol; // 768 pixels
-    final int screenHeight = tileSize * maxScreenRow; // 576 pixels
+    public final int maxScreenCol = 16;
+    public final int maxScreenRow = 12;
+    public final int screenWidth = tileSize * maxScreenCol; // 768 pixels
+    public final int screenHeight = tileSize * maxScreenRow; // 576 pixels
 
     // FPS
     int FPS = 60;
-
+    TileManager tileM = new TileManager(this);
     Keyhandler keyH = new Keyhandler();
     Thread gameThread;
     Player player = new Player(this, keyH);
 
     // Set player's default position
-    int playerX = 100;
-    int playerY = 100;
-    int playerSpeed = 4;
+    // int playerX = 100;
+    // int playerY = 100;
+    // int playerSpeed = 4;
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
@@ -47,36 +48,6 @@ public class GamePanel extends JPanel implements Runnable {
     }
 
     @Override
-    // public void run() {
-
-    // double drawInterval = 1000000000 / FPS; // 0.016666666 seconds
-    // double nextDrawTime = System.nanoTime() + drawInterval;
-
-    // while (gameThread != null) {
-
-    // update();
-
-    // repaint();
-
-    // try {
-    // double remainingTime = nextDrawTime - System.nanoTime();
-    // remainingTime = remainingTime / 1000000;
-
-    // if (remainingTime < 0) {
-    // remainingTime = 0;
-    // }
-
-    // Thread.sleep((long) remainingTime);
-
-    // nextDrawTime += drawInterval;
-
-    // } catch (InterruptedException e) {
-    // // TODO Auto-generated catch block
-    // e.printStackTrace();
-    // }
-    // }
-
-    // }
     public void run() {
 
         double drawInterval = 1000000000 / FPS;
@@ -110,6 +81,7 @@ public class GamePanel extends JPanel implements Runnable {
     }
 
     public void update() {
+
         player.update();
 
     }
@@ -119,7 +91,11 @@ public class GamePanel extends JPanel implements Runnable {
         super.paintComponent(g);
 
         Graphics2D g2 = (Graphics2D) g;
+
+        tileM.draw(g2);
+
         player.draw(g2);
+
         g2.dispose();
     }
 }
