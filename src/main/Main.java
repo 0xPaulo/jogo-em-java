62af8ae9d634e5fb2784a769f652c1c9694f38f2
0xPaulo <paulo_czar@outlook.com.br>
2023-04-08 00:29:06 -0300

    sprite-update

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,7 +7,7 @@ public class Main {
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
-        window.setTitle("2D Adenture");
+        window.setTitle("PaaAAaaPa Adenture");
 
         GamePanel gamePanel = new GamePanel();
         window.add(gamePanel);
